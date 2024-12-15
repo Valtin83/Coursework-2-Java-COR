5569bd3b0741c98575480616f61cd879ca0f68cf
zhe-v <zhe-valentin@mail.ru>
2024-12-15 15:54:01 +0700

    Курсовая 2

---
+++ Coursework-2-Java-COR/src/main/java/org/skypro/Coursework2/Java/COR/Coursework2JavaCorApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.Coursework2.Java.COR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2JavaCorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework2JavaCorApplication.class, args);
+	}
+
+}
